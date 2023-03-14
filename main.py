e0c6c852b4ef56b981513bdd2a8b5aaa239e6c4b
aymalgwi <malgwiabraham@gmail.com>
2023-03-15 00:41:04 +0100

    sd

--- main.py
+++ main.py
@@ -276,7 +276,13 @@ elif age > 17:
     print("Go to College")
 
 
+# find odd numbers from 1 - 20
 
+for number in range(1, 21):
+    if not (number % 2 == 0):
+        print("{} is odd".format(number))
+    else:
+        print(number)
 
 
 
--- newfile.py
+++
@@ -1,7 +0,0 @@
-# find odd numbers from 1 - 20
-
-for number in range(1, 21):
-    if not (number % 2 == 0):
-        print("{} is odd".format(number))
-    else:
-        print(number)
